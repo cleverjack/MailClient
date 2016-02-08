63080df03915fee1c76f33555db8db0b365f7867
John Mitnick <overkazaf@gmail.com>
2016-02-08 12:18:01 +0800

    add bs and fa libs

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,12 @@
 	var MailApp = {
 		init : function () {
 			console.log('app is initial');
+			this.bindEvents();
 		}, 
 		bindEvents : function () {
-			
+			$('#writeMail').on('click', function () {
+				$('#viewModal').modal('show');
+			});
 		}
 	};
 
